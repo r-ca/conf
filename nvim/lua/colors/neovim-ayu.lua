ecc8c22572a688255494f0b6f3df8cdac8c04392
r-ca <nem@nem.x0.to>
2024-05-12 18:21:13 +0900

    ayu iroiro

--- nvim/lua/colors/neovim-ayu.lua
+++ nvim/lua/colors/neovim-ayu.lua
@@ -1,7 +1,12 @@
 return {
     'Shatur/neovim-ayu',
-    setup = function({
-        mirage = true
-    })
+    config = function()
+        require('ayu').setup({
+            mirage = true,
+        })
+
+        -- add command
+        vim.api.nvim_create_user_command('AyuDark', 'colorscheme ayu-mirage', {})
+        vim.api.nvim_create_user_command('AyuLight', 'colorscheme ayu-light', {})
     end,
 }
