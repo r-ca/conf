dc96ea45936527460445d177105abeb547b8b7b2
r-ca <nem@nem.x0.to>
2024-05-12 18:48:03 +0900

    キーマップ追加

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,3 +27,17 @@ require('lazy').setup({
 
 -- set colorscheme
 vim.cmd('colorscheme ayu')
+
+-- Keymaps
+-- Window navigation
+-- in normal
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+
+-- in terminal
+vim.keymap.set('t', '<C-h>', '<C-\\><C-N>:wincmd h<CR>')
+vim.keymap.set('t', '<C-l>', '<C-\\><C-N>:wincmd l<CR>')
+vim.keymap.set('t', '<C-j>', '<C-\\><C-N>:wincmd j<CR>')
+vim.keymap.set('t', '<C-k>', '<C-\\><C-N>:wincmd k<CR>')
