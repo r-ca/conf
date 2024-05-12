7b2e917ca567cd80fc6c6ac7ba00b0ce9be41de5
r-ca <nem@nem.x0.to>
2024-05-12 18:50:56 +0900

    LazyGit移植

--- nvim/init.lua
+++ nvim/init.lua
@@ -41,3 +41,26 @@ vim.keymap.set('t', '<C-h>', '<C-\\><C-N>:wincmd h<CR>')
 vim.keymap.set('t', '<C-l>', '<C-\\><C-N>:wincmd l<CR>')
 vim.keymap.set('t', '<C-j>', '<C-\\><C-N>:wincmd j<CR>')
 vim.keymap.set('t', '<C-k>', '<C-\\><C-N>:wincmd k<CR>')
+
+-- LazyGit
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+
+function _lazygit_toggle()
+    lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap('n', 'tg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
+
+local group = vim.api.nvim_create_augroup("jump_last_position", { clear = true })
+vim.api.nvim_create_autocmd(
+	"BufReadPost",
+	{callback = function()
+			local row, col = unpack(vim.api.nvim_buf_get_mark(0, "\""))
+			if {row, col} ~= {0, 0} then
+				vim.api.nvim_win_set_cursor(0, {row, 0})
+			end
+		end,
+	group = group
+	}
+)
