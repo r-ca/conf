ba25ed6f4936a367f2249de93fc8e8301b6e74cb
r-ca <nem@nem.x0.to>
2024-05-12 16:28:15 +0900

    廃止

--- nvim/lua/utils/lsp-list.lua
+++
@@ -1,7 +0,0 @@
-return = function()
-    local clients = {}
-    for _, client in ipairs(vim.lsp.get_active_clients() { bufnr = 0 }) do
-        table.insert(clients, client.name)
-    end
-    return clients
-end
