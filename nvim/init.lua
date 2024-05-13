23dcf517943a3e6304db24a712ea4eda6a02b250
ろむねこ <nem@nem.x0.to>
2024-05-13 10:08:26 +0900

    macOSの場合はCommandでCtrl用のショートカットを呼び出すように

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,16 +31,31 @@ vim.cmd('colorscheme ayu')
 -- Keymaps
 -- Window navigation
 -- in normal
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+if vim.fn.has('mac') == 1 then
+    vim.keymap.set('n', '<D-h>', ':wincmd h<CR>')
+    vim.keymap.set('n', '<D-l>', ':wincmd l<CR>')
+    vim.keymap.set('n', '<D-j>', ':wincmd j<CR>')
+    vim.keymap.set('n', '<D-k>', ':wincmd k<CR>')
+else
+    vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+    vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+    vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+    vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+end
 
 -- in terminal
-vim.keymap.set('t', '<C-h>', '<C-\\><C-N>:wincmd h<CR>')
-vim.keymap.set('t', '<C-l>', '<C-\\><C-N>:wincmd l<CR>')
-vim.keymap.set('t', '<C-j>', '<C-\\><C-N>:wincmd j<CR>')
-vim.keymap.set('t', '<C-k>', '<C-\\><C-N>:wincmd k<CR>')
+if vim.fn.has('mac') == 1 then
+    vim.keymap.set('t', '<D-h>', '<C-\\><C-N>:wincmd h<CR>')
+    vim.keymap.set('t', '<D-l>', '<C-\\><C-N>:wincmd l<CR>')
+    vim.keymap.set('t', '<D-j>', '<C-\\><C-N>:wincmd j<CR>')
+    vim.keymap.set('t', '<D-k>', '<C-\\><C-N>:wincmd k<CR>')
+else
+    vim.keymap.set('t', '<C-h>', '<C-\\><C-N>:wincmd h<CR>')
+    vim.keymap.set('t', '<C-l>', '<C-\\><C-N>:wincmd l<CR>')
+    vim.keymap.set('t', '<C-j>', '<C-\\><C-N>:wincmd j<CR>')
+    vim.keymap.set('t', '<C-k>', '<C-\\><C-N>:wincmd k<CR>')
+end
+
 
 -- LazyGit
 local Terminal = require('toggleterm.terminal').Terminal
