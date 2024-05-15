485dbc703f1136232a40ca487b95b2ffea9106f3
r-ca <nem@nem.x0.to>
2024-05-15 11:43:41 +0900

    telescope追加

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+    },
+    config = function()
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<C-f>', builtin.find_files)
+        vim.keymap.set('n', '<C-g>', builtin.live_grep)
+    end
+}
