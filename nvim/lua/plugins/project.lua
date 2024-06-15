1e09500ef84dd013b195341e944a4877eb3ccd39
rca <nem@nem.x0.to>
2024-06-16 05:46:40 +0900

    telescope拡張に追加

--- nvim/lua/plugins/project.lua
+++ nvim/lua/plugins/project.lua
@@ -6,6 +6,7 @@ return {
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     }
+    require('telescope').load_extension('projects')
   end
 }
 
