330379a4165c6b7e58195876241820d6ce06794e
ろむねこ <nem@nem.x0.to>
2024-06-17 14:49:58 +0900

    neo-treeに変更

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,22 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function ()
+        require("neo-tree").setup({
+            window = {
+                mappings = {
+                    ["<SPACE>"] = "open",
+                }
+            }
+        });
+
+
+        vim.api.nvim_set_keymap('n', 'tf', '<cmd>Neotree toggle<CR>', { noremap = true, silent = true })
+    end
+}
