1180ff9e2d9f14ebaca7b62173c4891e2fedea6a
ろむねこ <nem@nem.x0.to>
2024-06-18 10:23:48 +0900

    ウィンドウ移動系をsilentに

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,10 +34,10 @@ vim.cmd('colorscheme ayu')
 -- Keymaps
 -- Window navigation
 -- in normal
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', {silent = true})
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', {silent = true})
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', {silent = true})
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', {silent = true})
 
 -- in terminal
 vim.keymap.set('t', '<C-h>', '<C-\\><C-N>:wincmd h<CR>')
