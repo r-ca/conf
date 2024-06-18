88432171776a110ca96e97ffbe553c61d0acf4fe
ろむねこ <nem@nem.x0.to>
2024-06-18 10:56:07 +0900

    image-preview(テスト)

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -12,8 +12,18 @@ return {
             window = {
                 mappings = {
                     ["<SPACE>"] = "open",
+                    ["<SPACE>p"] = "image_wezterm"
+
                 }
-            }
+            },
+            commands = {
+                image_wezterm = function(state)
+                    local node = state.tree:get_node()
+                    if node.type == "file" then
+                        require("image_preview").PreviewImage(node.path)
+                    end
+                end,
+            },
         });
 
 
