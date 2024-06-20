a12fc6d1e6b0299b11ac09059596e6e1cd7b0461
ろむねこ <nem@nem.x0.to>
2024-06-20 11:54:22 +0900

    overwrite警告が出ないように

--- nvim/lua/plugins/noice-nvim.lua
+++ nvim/lua/plugins/noice-nvim.lua
@@ -11,5 +11,17 @@ return {
         --   `nvim-notify` is only needed, if you want to use the notification view.
         --   If not available, we use `mini` as the fallback
         "rcarriga/nvim-notify",
-    }
+    },
+    config = function()
+        require("noice").setup({
+            lsp = {
+                signature = {
+                    enabled = false
+                },
+                hover = {
+                    enabled = false
+                },
+            },
+        })
+    end
 }
