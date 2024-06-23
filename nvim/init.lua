82c24859af32759da19973efd7adf7d550b3bcf1
rca <nem@nem.x0.to>
2024-06-23 15:51:22 +0900

    package-info-nvim追加

--- nvim/init.lua
+++ nvim/init.lua
@@ -68,6 +68,15 @@ if vim.fn.has('unix') == 1 then
     vim.api.nvim_set_keymap('c', '<S-Space>', '<C-C>', { noremap = true, silent = true })
 end
 
+-- AddCommands
+vim.api.nvim_create_user_command('PackageInfo',
+    function(opts)
+        require('package-info').show({force = true})
+    end, {
+        nargs = 0
+    }
+)
+
 -- LazyGit
 local Terminal = require('toggleterm.terminal').Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
