c22c422be94fb4f87394209abc40ab31ea3ffccd
ろむねこ <nem@nem.x0.to>
2024-06-24 15:33:24 +0900

    fix

--- nvim/lua/lsp/none-ls.lua
+++ nvim/lua/lsp/none-ls.lua
@@ -9,9 +9,9 @@ return {
         require("null-ls").setup({
             sources = {
                 -- ESLint
-                require("none-ls.builtins.diagnostics.eslint_d"),
-                require("none-ls.builtins.code_actions.eslint_d"),
-                require("none-ls.builtins.formatting.eslint_d"),
+                require("none-ls.diagnostics.eslint_d"),
+                require("none-ls.code_actions.eslint_d"),
+                require("none-ls.formatting.eslint_d"),
                 -- Pritter
                 null_ls.builtins.formatting.prettierd,
             }
