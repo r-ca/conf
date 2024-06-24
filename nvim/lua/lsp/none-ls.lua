b769230333efd57dae6728811684e46cc516d25d
ろむねこ <nem@nem.x0.to>
2024-06-24 15:19:41 +0900

    fix typo

--- nvim/lua/lsp/none-ls.lua
+++ nvim/lua/lsp/none-ls.lua
@@ -5,7 +5,7 @@ return {
         require("null-ls").setup({
             sources = {
                 -- ESLint
-                null_ls.builtins.diagnostic.eslint_d,
+                null_ls.builtins.diagnostics.eslint_d,
                 null_ls.builtins.code_actions.eslint_d,
                 null_ls.builtins.formatting.eslint_d,
                 -- Pritter
