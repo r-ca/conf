290b5065c5f9ea53c94586b0def09a72de908dde
ろむねこ <nem@nem.x0.to>
2024-06-24 16:19:17 +0900

    Revert "mason-null-ls"
    This reverts commit 61c4a30bb9b6b21d3bd7808d95c051305f55bfa1.

--- nvim/lua/lsp/none-ls.lua
+++ nvim/lua/lsp/none-ls.lua
@@ -5,16 +5,16 @@ return {
         "nvim-lua/plenary.nvim"
     },
     config = function()
-        -- local null_ls = require("null-ls");
-        -- require("null-ls").setup({
-            -- sources = {
-            --     -- ESLint
-            --     require("none-ls.diagnostics.eslint_d"),
-            --     require("none-ls.code_actions.eslint_d"),
-            --     require("none-ls.formatting.eslint_d"),
-            --     -- Pritter
-            --     null_ls.builtins.formatting.prettierd,
-            -- }
-        -- })
+        local null_ls = require("null-ls");
+        require("null-ls").setup({
+            sources = {
+                -- ESLint
+                require("none-ls.diagnostics.eslint_d"),
+                require("none-ls.code_actions.eslint_d"),
+                require("none-ls.formatting.eslint_d"),
+                -- Pritter
+                null_ls.builtins.formatting.prettierd,
+            }
+        })
     end,
 }
