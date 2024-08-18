a73b77ca6b2381c2ba565229a8df507c64a0fc0a
rca <nem@nem.x0.to>
2024-08-18 20:06:05 +0900

    allにした

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -4,7 +4,8 @@ return {
 		config = function()
 			local configs = require("nvim-treesitter.configs")
 			configs.setup({
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+				-- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+                ensure_installed = "all",
 				sync_install = false,
 				highlight = { enable = true },
 				indent = { enable = true },
