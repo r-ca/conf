3c619ebf088730fffee95780a54c495782fc9f36
rca <nem@nem.x0.to>
2024-09-17 02:06:00 +0900

    wip

--- nvim/lua/plugins/context-menu.lua
+++ nvim/lua/plugins/context-menu.lua
@@ -8,16 +8,44 @@ return {
                     order = 1,
                     cmd = "Hello world!",
                     action = {
-                        type = "function",
-                        fn = function()
+                        type = "callback",
+                        callback = function()
                             print("Hello world!")
                         end
                     }
+                },
+                {
+                    order = 2,
+                    cmd = "Hello world 2!",
+                    action = {
+                        type = "sub_cmds",
+                        sub_cmds = {
+                            {
+                                cmd = "Hello world 2.1!",
+                                action = {
+                                    type = "callback",
+                                    callback = function()
+                                        print("Hello world 2.1!")
+                                    end
+                                }
+                            },
+                            {
+                                cmd = "Hello world 2.2!",
+                                action = {
+                                    type = "callback",
+                                    callback = function()
+                                        print("Hello world 2.2!")
+                                    end
+                                }
+                            }
+                        }
+                    },
                 }
             }
         })
 
         -- Keymaps
-        vim.api.nvim_set_keymap('n', '<F9>', "<cmd> lua require('context-menu').trigger_context_menu() <cr>", { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<Space>', "<cmd> lua require('context-menu').trigger_context_menu() <cr>",
+            { noremap = true, silent = true })
     end
 }
