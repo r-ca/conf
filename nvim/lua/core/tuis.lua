95ca1ffad5e2b24bee517924f06aa92617748add
rca <nem@nem.x0.to>
2024-09-26 12:27:20 +0900

    TUI移植

--- nvim/lua/core/tuis.lua
+++ nvim/lua/core/tuis.lua
@@ -1,1 +1,20 @@
 -- TUI utilities
+local M = {}
+
+local Terminal = require('toggleterm.terminal').Terminal
+
+-- LazyDocker
+local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true, direction = "float" })
+
+function M.LazyDockerToggle()
+    lazydocker:toggle()
+end
+
+-- LazyGit
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+
+function M.LazyGitToggle()
+    lazygit:toggle()
+end
+
+return M
