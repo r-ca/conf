f6d8c738d28e593ae21d275ff26bafd4cf553342
rca <nem@nem.x0.to>
2024-09-26 12:35:25 +0900

    remove dup

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,19 +31,6 @@ require('lazy').setup({
 -- set colorscheme
 vim.cmd('colorscheme ayu')
 
--- Keymaps
--- Window navigation
--- in normal
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', { silent = true })
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', { silent = true })
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', { silent = true })
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', { silent = true })
-
--- in terminal
-vim.keymap.set('t', '<C-h>', '<C-\\><C-N>:wincmd h<CR>')
-vim.keymap.set('t', '<C-l>', '<C-\\><C-N>:wincmd l<CR>')
-vim.keymap.set('t', '<C-j>', '<C-\\><C-N>:wincmd j<CR>')
-vim.keymap.set('t', '<C-k>', '<C-\\><C-N>:wincmd k<CR>')
 
 -- undo
 vim.api.nvim_set_keymap('n', '<C-z>', '<cmd>undo<CR>', { noremap = true, silent = true })
@@ -51,24 +38,6 @@ vim.api.nvim_set_keymap('n', '<C-z>', '<cmd>undo<CR>', { noremap = true, silent 
 vim.api.nvim_set_keymap('n', '<C-n>', '<cmd>tabNext<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-c>', '<cmd>tabclose<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-t>', '<cmd>tabnew<CR>', { noremap = true, silent = true })
-
--- LSP Action(Workaround)
-vim.api.nvim_set_keymap('n', '<Space>', '<cmd>Lspsaga hover_doc<CR>', { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<CR>', '<cmd>Lspsaga signature_help<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'gd', '<cmd>Lspsaga preview_definition<CR>', { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', 'gr', '<cmd>Lspsaga rename<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'gh', '<cmd>Lspsaga finder<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'ga', '<cmd>Lspsaga code_action<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'gs', '<cmd>Lspsaga show_line_diagnostics<CR>', { noremap = true, silent = true })
--- format
-vim.api.nvim_set_keymap('n', 'gf', '<cmd>lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-CR>', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
-
--- Clipboard
-vim.api.nvim_set_keymap('n', 'cp', '"*p', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'cy', '"*y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', 'cy', '"*y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', 'cp', '"*p', { noremap = true, silent = true })
 
 -- AddCommands
 vim.api.nvim_create_user_command('PackageInfo',
@@ -78,27 +47,6 @@ vim.api.nvim_create_user_command('PackageInfo',
         nargs = 0
     }
 )
-
--- TUI Utils
-local Terminal = require('toggleterm.terminal').Terminal
-
--- LazyDocker
-local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true, direction = "float" })
-
-function _lazydocker_toggle()
-    lazydocker:toggle()
-end
-
-vim.api.nvim_set_keymap('n', 'td', '<cmd>lua _lazydocker_toggle()<CR>', { noremap = true, silent = true })
-
--- LazyGit
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
-
-function _lazygit_toggle()
-    lazygit:toggle()
-end
-
-vim.api.nvim_set_keymap('n', 'tg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
 
 -- Restore cursor position
 local group = vim.api.nvim_create_augroup("jump_last_position", { clear = true })
