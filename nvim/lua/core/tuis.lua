15dfb6ea2abe59954f01f1edc18eb40499fbc59a
rca <nem@nem.x0.to>
2024-09-26 12:31:19 +0900

    関数を返す関数に

--- nvim/lua/core/tuis.lua
+++ nvim/lua/core/tuis.lua
@@ -6,15 +6,23 @@ local Terminal = require('toggleterm.terminal').Terminal
 -- LazyDocker
 local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true, direction = "float" })
 
-function M.LazyDockerToggle()
+local function _lazydocker_toggle()
     lazydocker:toggle()
+end
+
+function M.lazydocker_toggle()
+    return _lazydocker_toggle()
 end
 
 -- LazyGit
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
 
-function M.LazyGitToggle()
+local function _lazygit_toggle()
     lazygit:toggle()
+end
+
+function M.lazygit_toggle()
+    return _lazygit_toggle()
 end
 
 return M
