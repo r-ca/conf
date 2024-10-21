51606e2fe8693e850aa6dcf0f0dc55bfe7801213
rca <nem@nem.x0.to>
2024-10-21 11:45:30 +0900

    barbarマップ移植

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -30,6 +30,17 @@ kmap.normal('gs', 'Lspsaga show_line_diagnostics')
 kmap.normal('gf', 'vim.lsp.buf.format()')
 kmap.normal('<S-CR>', 'vim.lsp.buf.definition()')
 
+-- Change buffer
+vim.api.nvim_set_keymap('n', '<C-]>', ':BufferNext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-[>', ':BufferPrevious<CR>', { noremap = true, silent = true })
+-- Reorder buffer
+vim.api.nvim_set_keymap('n', '<C-S-]>', ':BufferMoveNext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-S-[>', ':BufferMovePrevious<CR>', { noremap = true, silent = true })
+-- Close buffer
+vim.api.nvim_set_keymap('n', '<C-i>', ':BufferClose<CR>', { noremap = true, silent = true })
+-- Restore buffer
+vim.api.nvim_set_keymap('n', '<C-S-i>', ':BufferRestore<CR>', { noremap = true, silent = true })
+
 -- Clipboard
 kmap.set({Mode.NORMAL, Mode.VISUAL}, 'cp', '"*p')
 kmap.set({Mode.NORMAL, Mode.VISUAL}, 'cy', '"*y')
--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -25,15 +25,5 @@ return {
 		},
     },
     config = function()
-        -- Change buffer
-        vim.api.nvim_set_keymap('n', '<C-]>', ':BufferNext<CR>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<C-[>', ':BufferPrevious<CR>', { noremap = true, silent = true })
-        -- Reorder buffer
-        vim.api.nvim_set_keymap('n', '<C-S-]>', ':BufferMoveNext<CR>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<C-S-[>', ':BufferMovePrevious<CR>', { noremap = true, silent = true })
-        -- Close buffer
-        vim.api.nvim_set_keymap('n', '<C-i>', ':BufferClose<CR>', { noremap = true, silent = true })
-        -- Restore buffer
-        vim.api.nvim_set_keymap('n', '<C-S-i>', ':BufferRestore<CR>', { noremap = true, silent = true })
     end
 }
