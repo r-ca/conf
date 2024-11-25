0d3abcf29fef9aa5eb76670e8920955ed720a8a0
rca <nem@nem.x0.to>
2024-11-25 16:48:02 +0900

    add_to_path wip

---
+++ zsh/zsh.d/utils.zsh
@@ -0,0 +1,17 @@
+add_to_path() {
+  local dir="$1"
+
+  # Check if the directory exists
+  if [ ! -d "$dir" ]; then
+    echo "Directory does not exist: $dir" >&2
+    return 1
+  fi
+
+  # Check if the directory is already in PATH
+  if [[ ":$PATH:" != *":$dir:"* ]]; then
+    export PATH="$dir:$PATH"
+  else
+    echo "Already in PATH: $dir"
+  fi
+}
+
