fe2c09a14222f980e4da430c00ccce8b819f03c0
rca <nem@nem.x0.to>
2024-12-02 16:16:56 +0900

    format

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -4,7 +4,7 @@ return {
     branch = "canary",
     dependencies = {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+      { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
     },
     opts = {
       debug = true, -- Enable debugging
