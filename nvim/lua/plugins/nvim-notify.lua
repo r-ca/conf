e9252f2406253dbe63d4648585a9b4258b6d9e0f
rca <nem@nem.x0.to>
2024-12-02 16:17:58 +0900

    format

--- nvim/lua/plugins/nvim-notify.lua
+++ nvim/lua/plugins/nvim-notify.lua
@@ -1,14 +1,14 @@
 return {
-    "rcarriga/nvim-notify",
-    config = function()
-        local nvim_notify = require("notify")
-        nvim_notify.setup {
-            -- Animation style
-            stages = "fade_in_slide_out",
-            -- Default timeout for notifications
-            timeout = 1500,
-            render = "default"
-        }
-        vim.notify = nvim_notify
-    end
+  "rcarriga/nvim-notify",
+  config = function()
+    local nvim_notify = require("notify")
+    nvim_notify.setup {
+      -- Animation style
+      stages = "fade_in_slide_out",
+      -- Default timeout for notifications
+      timeout = 1500,
+      render = "default"
+    }
+    vim.notify = nvim_notify
+  end
 }
