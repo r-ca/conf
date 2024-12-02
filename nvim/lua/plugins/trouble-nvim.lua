54b19cf5fe1976a0ecdfaf7c783aacadca777417
rca <nem@nem.x0.to>
2024-12-02 16:18:08 +0900

    format

--- nvim/lua/plugins/trouble-nvim.lua
+++ nvim/lua/plugins/trouble-nvim.lua
@@ -1,15 +1,15 @@
 return {
-    "folke/trouble.nvim",
-    opts = {},
-    cmd = "Trouble",
-    keys = {
-        {
-            "<F5>",
-            "<cmd>Trouble diagnostics toggle<cr>",
-        },
-        {
-            "<F6>",
-            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>"
-        },
+  "folke/trouble.nvim",
+  opts = {},
+  cmd = "Trouble",
+  keys = {
+    {
+      "<F5>",
+      "<cmd>Trouble diagnostics toggle<cr>",
     },
+    {
+      "<F6>",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>"
+    },
+  },
 }
