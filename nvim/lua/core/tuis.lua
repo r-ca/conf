c4e4f2bb8ce1fd4f3a7196f64e4be6278568dabc
rca <nem@nem.x0.to>
2024-12-11 06:17:45 +0900

    indent

--- nvim/lua/core/tuis.lua
+++ nvim/lua/core/tuis.lua
@@ -7,22 +7,22 @@ local Terminal = require('toggleterm.terminal').Terminal
 local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true, direction = "float" })
 
 local function _lazydocker_toggle()
-    lazydocker:toggle()
+  lazydocker:toggle()
 end
 
 function M.lazydocker_toggle()
-    return _lazydocker_toggle
+  return _lazydocker_toggle
 end
 
 -- LazyGit
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
 
 local function _lazygit_toggle()
-    lazygit:toggle()
+  lazygit:toggle()
 end
 
 function M.lazygit_toggle()
-    return _lazygit_toggle
+  return _lazygit_toggle
 end
 
 return M
