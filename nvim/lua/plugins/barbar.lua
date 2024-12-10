54d43d2ed558af60ce7a297a7eabb568dbf571ea
rca <nem@nem.x0.to>
2024-12-11 06:18:13 +0900

    indent

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -1,29 +1,29 @@
 return {
-    'romgrk/barbar.nvim',
-    dependencies = {
-        'nvim-tree/nvim-web-devicons',
-        'lewis6991/gitsigns.nvim'
+  'romgrk/barbar.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'lewis6991/gitsigns.nvim'
+  },
+  opts = {
+    animation = true,
+    gitsigns = {
+      added = {
+        enabled = true,
+        icon = '+'
+      },
+      changed = {
+        enabled = true,
+        icon = '~'
+      },
+      deleted = {
+        enabled = true,
+        icon = '-'
+      },
     },
-    opts = {
-        animation = true,
-		gitsigns = {
-			added = {
-				enabled = true,
-				icon = '+'
-			},
-			changed = {
-				enabled = true,
-				icon = '~'
-			},
-			deleted = {
-				enabled = true,
-				icon = '-'
-			},
-		},
-		sidebar_filetypes = {
-			NvimTree = true,
-		},
+    sidebar_filetypes = {
+      NvimTree = true,
     },
-    config = function()
-    end
+  },
+  config = function()
+  end
 }
