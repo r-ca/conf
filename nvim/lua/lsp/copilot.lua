2d74941c9396b0e4aae9bad23f7bf0feca818bfe
rca <nem@nem.x0.to>
2024-12-11 11:47:16 +0900

    indent

--- nvim/lua/lsp/copilot.lua
+++ nvim/lua/lsp/copilot.lua
@@ -1,15 +1,15 @@
 return {
-    'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    event = "InsertEnter",
-    config = function()
-        require('copilot').setup({
-            panel = {
-                enabled = false,
-            },
-            suggestion = {
-                enabled = false,
-            }
-        })
-    end
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = "InsertEnter",
+  config = function()
+    require('copilot').setup({
+      panel = {
+        enabled = false,
+      },
+      suggestion = {
+        enabled = false,
+      }
+    })
+  end
 }
