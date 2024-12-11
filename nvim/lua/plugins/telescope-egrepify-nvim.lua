0afe712372d09c54a62d82086a92bf6d076addef
rca <nem@nem.x0.to>
2024-12-11 12:17:03 +0900

    add egrepify

---
+++ nvim/lua/plugins/telescope-egrepify-nvim.lua
@@ -0,0 +1,7 @@
+return {
+  "fdschmidt93/telescope-egrepify.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  config = function()
+    require("telescope").load_extension("egrepify")
+  end,
+}
