f443163640238ceb49062389041e31438b9771fb
rca <nem@nem.x0.to>
2024-12-11 11:46:38 +0900

    workaround

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -5,7 +5,8 @@ return {
     "nvim-lua/plenary.nvim",
     "antoinemadec/FixCursorHold.nvim",
     "nvim-treesitter/nvim-treesitter",
-    "marilari88/neotest-vitest"
+    -- "marilari88/neotest-vitest"
+    { dir = "/Users/rca/proj/neotest-vitest" }
   },
   config = function()
     require("neotest").setup({
