4d450c82aec008baaa8725bb9109b5d12a73e887
rca <nem@nem.x0.to>
2024-12-12 10:09:45 +0900

    loader設定 wip

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -3,5 +3,8 @@ return {
   -- follow latest release.
   version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
   -- install jsregexp (optional!).
-  build = "make install_jsregexp"
+  build = "make install_jsregexp",
+  config = function()
+    require("luasnip.loaders.from_lua").load()
+  end,
 }
