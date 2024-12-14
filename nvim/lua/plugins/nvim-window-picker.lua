2c348327f9fa91ef0456e674d9ff9954a99c3b48
rca <nem@nem.x0.to>
2024-12-14 19:36:48 +0900

    workaround?

--- nvim/lua/plugins/nvim-window-picker.lua
+++ nvim/lua/plugins/nvim-window-picker.lua
@@ -4,6 +4,8 @@ return {
   event = 'VeryLazy',
   version = '2.*',
   config = function()
-    require 'window-picker'.setup()
+    require 'window-picker'.setup({
+      hint = 'floating-big-letter',
+    })
   end,
 }
