755a6cf07c7a40bfd8f4628245e2d4b2595a137f
rca <nem@nem.x0.to>
2024-12-14 20:07:55 +0900

    nvim-window-pickerをchowchoで代替

--- nvim/lua/plugins/chowcho-nvim.lua
+++ nvim/lua/plugins/chowcho-nvim.lua
@@ -2,5 +2,16 @@ return {
   "tkmpypy/chowcho.nvim",
   config = function()
     require("chowcho").setup({})
+
+    -- nvim-window-pickerの代替として動作させる
+    package.loaded['window-picker'] = {
+      pick_window = function()
+        local selected_window_id
+        require('chowcho').run(function(window_id)
+          selected_window_id = window_id
+        end)
+        return selected_window_id
+      end
+    }
   end,
 }
