c7dea1d8c76a0b93efc707c1586a2d6e3a7cd24f
rca <nem@nem.x0.to>
2024-12-14 19:36:56 +0900

    telescope-menu wip

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,5 +5,27 @@ return {
     'nvim-lua/plenary.nvim',
   },
   config = function()
+    require('telescope').setup {
+      defaults = {
+        layout_config = {
+          prompt_position = 'top',
+        },
+      },
+      extensions = {
+        menu = {
+          default = {
+            items = {
+              { "Copilot Chat", "Telescope menu copilot" },
+            },
+          },
+          copilot = {
+            items = {
+              { "Open", "CopilotChatOpen" },
+              { "Close", "CopilotChatClose" },
+            }
+          }
+        },
+      },
+    }
   end
 }
