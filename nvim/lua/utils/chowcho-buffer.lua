594b1bf7028ce633b389b324a6cfc38b8661cd94
rca <nem@nem.x0.to>
2024-12-14 18:02:25 +0900

    fix test

--- nvim/lua/utils/chowcho-buffer.lua
+++ nvim/lua/utils/chowcho-buffer.lua
@@ -1,43 +1,38 @@
 local M = {}
 
-local chowcho_run = require('chowcho').run
+local _chowcho_run = require('chowcho').run
 
--- 現在のバッファ番号とオプションを取得する関数
-local function get_bufnr_and_opt(winid)
+-- 現在のウィンドウのバッファ番号とローカルオプションを取得
+M.get_bufnr_and_opt = function(winid)
   return vim.api.nvim_win_call(winid, function()
     local bufnr = vim.fn.bufnr('%')
-    local opt_local = vim.opt_local:get()
-    return bufnr, opt_local
+    return bufnr, vim.opt_local
   end)
 end
 
--- バッファとオプションを設定する関数
-local function set_bufnr_and_opt(winid, bufnr, opt_local)
+-- 指定ウィンドウにバッファとローカルオプションを設定
+M.set_bufnr_and_opt = function(winid, bufnr, opt_local)
   return vim.api.nvim_win_call(winid, function()
     local old_bufnr = vim.fn.bufnr('%')
     vim.cmd("buffer " .. bufnr)
-    for key, value in pairs(opt_local) do
-      vim.opt_local[key] = value
-    end
+    vim.opt_local = opt_local
     return old_bufnr
   end)
 end
 
--- バッファを入れ替えるメイン関数
-function M.swap_buffers()
-  chowcho_run(function(n)
+-- バッファとローカルオプションを交換する
+M.swap_buffers = function()
+  _chowcho_run(function(n)
     if vim.fn.winnr('$') <= 2 then
       vim.cmd("wincmd x")
       return
     end
 
-    -- 現在のウィンドウ（0）と対象ウィンドウ（n）のバッファとオプションを取得
-    local bufnr0, opt_local0 = get_bufnr_and_opt(0)
-    local bufnrn, opt_localn = get_bufnr_and_opt(n)
+    local bufnr0, opt_local0 = M.get_bufnr_and_opt(0)
+    local bufnrn, opt_localn = M.get_bufnr_and_opt(n)
 
-    -- バッファとオプションを入れ替える
-    set_bufnr_and_opt(n, bufnr0, opt_local0)
-    set_bufnr_and_opt(0, bufnrn, opt_localn)
+    M.set_bufnr_and_opt(n, bufnr0, opt_local0)
+    M.set_bufnr_and_opt(0, bufnrn, opt_localn)
   end)
 end
 
