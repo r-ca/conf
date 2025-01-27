84ca84092643a2845dc34cfaadd685037fc930df
rca <nem@nem.x0.to>
2025-01-27 10:53:37 +0900

    use path arg

--- custom_scripts/bin/run_completion.sh
+++ custom_scripts/bin/run_completion.sh
@@ -3,7 +3,7 @@
 _nyashi() {
     local commands=("list" "run" "help")
     local scripts
-    scripts=($(ls "$HOME/scripts"/*.sh 2>/dev/null | xargs -n 1 basename | sed 's/\.sh$//'))
+    scripts=($(ls "$SCRIPT_DIR"/*.sh 2>/dev/null | xargs -n 1 basename | sed 's/\.sh$//'))
 
     _arguments \
         '1:command:(list run help)' \
