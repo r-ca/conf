326cb0f6f3c1d5b9f296d39638670377f92ab73f
rca <nem@nem.x0.to>
2025-04-22 16:44:58 +0900

    add template

--- nvim/lua/core/overseer.lua
+++ nvim/lua/core/overseer.lua
@@ -1,3 +1,8 @@
 require("overseer").setup({
-  templates = { "builtin", "user.lang.c.build_gcc" },
+  templates = { 
+    "builtin",
+    "user.lang.c.build_gcc",
+    "user.lang.java.build_javac",
+    "user.lang.python.run_python3",
+  },
 })
