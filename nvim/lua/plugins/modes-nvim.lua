26f9508315c6cb2082d88bf02005c1b8c852772a
rca <nem@nem.x0.to>
2025-06-06 06:07:00 +0900

    fix

--- nvim/lua/plugins/modes-nvim.lua
+++ nvim/lua/plugins/modes-nvim.lua
@@ -4,7 +4,7 @@ return {
     require('modes').setup({
       -- Disable modes highlights in specified filetypes
       -- Please PR commonly ignored filetypes
-      ignore_filetypes = { 'NvimTree', 'TelescopePrompt' }
+      ignore = { 'NvimTree', 'TelescopePrompt' }
     })
   end
 }
