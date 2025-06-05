89477eac2be236541ed91b073f82f15cc1a7e336
rca <_@r0m.me>
2025-06-05 15:52:23 +0900

    format

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,14 @@
 -- Bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",     -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -17,12 +17,12 @@ vim.opt.termguicolors = true
 
 -- Load plugins dir
 require('lazy').setup({
-    spec = {
-        { import = "plugins" },
-        { import = "lsp" },
-        { import = "lsp.lang" },
-        { import = "colors" }
-    }
+  spec = {
+    { import = "plugins" },
+    { import = "lsp" },
+    { import = "lsp.lang" },
+    { import = "colors" }
+  }
 })
 
 -- Load core
