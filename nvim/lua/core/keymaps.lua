4a7120294a941c562ccddbff6883d825835d1064
rca <_@r0m.me>
2025-06-24 15:47:30 +0900

    信じられない

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -147,10 +147,10 @@ function F.common()
   kmap.insert('<C-o>', '<Esc>o', { _autoCmd = false })
 
   -- Move cursor in insert mode
-  kmap.insert('<C-h>', '<Left>', { _autoCmd = false })
-  kmap.insert('<C-l>', '<Right>', { _autoCmd = false })
-  kmap.insert('<C-j>', '<Down>', { _autoCmd = false })
-  kmap.insert('<C-k>', '<Up>', { _autoCmd = false })
+  -- kmap.insert('<C-h>', '<Left>', { _autoCmd = false })
+  -- kmap.insert('<C-l>', '<Right>', { _autoCmd = false })
+  -- kmap.insert('<C-j>', '<Down>', { _autoCmd = false })
+  -- kmap.insert('<C-k>', '<Up>', { _autoCmd = false })
 end
 
 function F.overseer()
