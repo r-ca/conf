564b3cce2f17fe289f7a3fb15587f8930b004d89
rca <nem@nem.x0.to>
2026-02-03 09:57:26 +0900

    BackspaceキーをLeaderとして使用するように

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,17 @@
+-- Change leader to BS
+vim.g.mapleader = vim.api.nvim_replace_termcodes('<BS>', false, false, true)
+
 -- Bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",     -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -17,14 +20,14 @@ vim.opt.termguicolors = true
 
 -- Load plugins dir
 require('lazy').setup({
-  spec = {
-    { import = "plugins" },
-    { import = "lsp" },
-    { import = "lsp.lang" },
-    { import = "dap" },
-    { import = "dap.lang.extensions" },
-    { import = "colors" }
-  }
+    spec = {
+        { import = "plugins" },
+        { import = "lsp" },
+        { import = "lsp.lang" },
+        { import = "dap" },
+        { import = "dap.lang.extensions" },
+        { import = "colors" }
+    }
 })
 
 -- Load core
