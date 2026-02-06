97d79ceecca6a97f77888d78da365ddc32dfffbf
rca <nem@nem.x0.to>
2026-02-07 02:40:17 +0900

    cleanup?

--- nvim/lua/core/overseer.lua
+++ nvim/lua/core/overseer.lua
@@ -1,16 +1,10 @@
 require("overseer").setup({
-  templates = {
-    "builtin",
-    "user.lang.c.build_gcc",
-    "user.lang.java.build_javac",
-    "user.lang.python.run_python3",
-  },
-  task_list = {
-    bindings = {
-      ["<C-h>"] = false,
-      ["<C-l>"] = false,
-      ["<C-j>"] = false,
-      ["<C-k>"] = false,
+    templates = {
+        "builtin",
+        "user.lang.c.build_gcc",
+        "user.lang.java.build_javac",
+        "user.lang.python.run_python3",
+    },
+    task_list = {
     }
-  }
 })
