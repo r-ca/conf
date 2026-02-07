57849fc7d475d85bbfc8fe8d82c01b095ed9abcf
rca <nem@nem.x0.to>
2026-02-07 13:36:24 +0900

    testing

--- nvim/lua/lsp/lspconfig.lua
+++ nvim/lua/lsp/lspconfig.lua
@@ -3,7 +3,8 @@ return {
   dependencies = {
     'mason-org/mason.nvim',
     'mason-org/mason-lspconfig.nvim',
-    'folke/neoconf.nvim'
+    'folke/neoconf.nvim',
+    'mrjones2014/codesettings.nvim',
   },
 
   config = function()
@@ -24,7 +25,7 @@ return {
         -- JavaScript/TypeScript and related
         'ts_ls',
         'prismals',
-        'tailwindcss',
+        -- 'tailwindcss',
         'vue_ls',
         -- Rust
         'rust_analyzer',
@@ -42,7 +43,7 @@ return {
         exclude = {
           'jdtls' -- lang/nvim-jdtlsでセットアップしているので, TODO: 統合する
         }
-      }
+      },
     }
 
     -- TODO: 別のファイルに分割する?, /lspディレクトリを使って設定できるようになったらしいのでその方法にする?
@@ -52,6 +53,19 @@ return {
 
     -- Common
     vim.lsp.config('*', {
+      before_init = function(_, config)
+        local codesettings = require('codesettings')
+        codesettings.with_local_settings(config.name, config)
+
+        -- local settings = codesettings.local_settings()
+        -- local disabled = settings:get('disabledLs')
+        -- vim.notify(vim.inspect(disabled))
+        -- vim.notify(vim.inspect(config.name))
+        -- if disabled and vim.tbl_contains(disabled, config.name) then
+        --   vim.notify(string.format("LSP '%s' is disabled by codesettings.nvim", config.name), vim.log.levels.INFO)
+        --   return false  -- Prevent LSP from starting
+        -- end
+      end,
       capabilities = capabilities,
       on_attach = on_attach,
     })
---
+++ nvim/lua/plugins/codesettings-nvim.lua
@@ -0,0 +1,7 @@
+return {
+  'mrjones2014/codesettings.nvim',
+  config = function()
+    require('codesettings').setup {
+    }
+  end
+}
