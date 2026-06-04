c3d6be9eca9387189fd448a47e4b0fa85c82e8aa
rca <_@r0m.me>
2026-06-04 16:43:06 +0900

    補完が正しく動作するように？

--- zsh/zsh.d/common/run-nanoka.zsh
+++ zsh/zsh.d/common/run-nanoka.zsh
@@ -1,5 +1,8 @@
 export SCRIPT_DIR="$HOME/custom_scripts/scripts"
 export NANOKA_SCRIPT_PATH="$HOME/custom_scripts/bin/nanoka"
 fpath=( "$HOME/custom_scripts/completion" "${fpath[@]}" )
+
+autoload -Uz _nanoka
+
 alias nanoka="$NANOKA_SCRIPT_PATH"
 compdef _nanoka nanoka
