6c6bafed2395d15e16c6644328e854a42931efd3
rca <_@r0m.me>
2026-06-04 16:32:15 +0900

    パス修正

--- zsh/zsh.d/common/run-nanoka.zsh
+++ zsh/zsh.d/common/run-nanoka.zsh
@@ -1,5 +1,5 @@
 export SCRIPT_DIR="$HOME/custom_scripts/scripts"
-export NANOKA_SCRIPT_PATH="$HOME/custom_scripts/bin/nanoka.sh"
+export NANOKA_SCRIPT_PATH="$HOME/custom_scripts/bin/nanoka"
 fpath=( "$HOME/custom_scripts/completion" "${fpath[@]}" )
 alias nanoka="$NANOKA_SCRIPT_PATH"
 compdef _nanoka nanoka
